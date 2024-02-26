6cfbdf7c97f68c8018b559628618d2978e8487c1
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-02-26 17:09:02 +0000

    save changes

--- hello.py
+++ hello.py
@@ -1,30 +1,28 @@
+from bson import ObjectId
 from flask import Flask, redirect, render_template, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, template_folder='template')
-
-# models
-users = [{"id": 1, 'name': 'mahmoud', 'age': 27, 'location': 'bani suef'},
-         {"id": 2, 'name': 'mina', 'age': 30, 'location': 'cairo'}]
-
+app.config["MONGO_URI"]="mongodb://127.0.0.1:27017/flask"
+mongo=PyMongo(app)
+users=list(mongo.db.Users.find({}))
 
 
-#
-def get_id():
-    if users:
-        latest_user = users[-1]['id']
-        return latest_user
-    return 1
-
+# def get_id():
+#     if users:
+#         latest_user = users[-1]['id']
+#         return latest_user
+#     return 1
 
 @app.route("/")
 def hello_world():
+    data = list(mongo.db.Users.find({}))
+    print(data)
     return render_template('index.html')
-
 
 @app.route('/createuser')
 def createuser():
     return render_template('createuser.html')
-
 
 @app.route('/users', methods=['GET', 'POST'])
 def adduser():
@@ -32,66 +30,43 @@ def adduser():
         name = request.form.get('name')
         age = request.form.get('age')
         location = request.form.get('location')
-        users.append({'id': get_id() + 1, 'name': name, 'age': age, 'location': location})
-    return render_template('users.html', users=users)
-
-
-
-@app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
-def edit_user(id):
-    user = None
-    for u in users:
-        if u['id'] == id:
-            user = u
-            break
-
-    if user is None:
-        return 'User not found', 404
-    
-    if request.method == 'POST':
-        user['name'] = request.form.get('name')
-        user['location'] = request.form.get('location')
-        user['age'] = request.form.get('age')
-        return redirect('/users')
-    
-    return render_template('edit_user.html', user=user)
-
-
-@app.route('/delete_user/<int:id>')
-def delete_user(id):
-    if id is not None and len(users) != 0:
-        for i in range(len(users)):
-            if users[i]['id'] == id:
-                del users[i]
-                print("Found and Delete")
-                break
-    return redirect('/users')
-
+        if name and age and location: 
+            mongo.db.Users.insert_one({'name': name, 'age': age, 'location': location})
+    data = list(mongo.db.Users.find({}))  
+    return render_template('users.html', users=data) 
 
 @app.route('/users_list')
 def user_list():
-    return render_template('users.html', users=users)
+    data = list(mongo.db.Users.find({}))
+    return render_template('users.html', users=data)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
-
-
-# @app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
-# def edit_user(id):
-#     user = next((u for u in users if u['id'] == id), None)
-#     if user is None:
-#         return 'User not found', 404
-    
+# @app.route('/edit_user/<string:_id>', methods=['GET', 'POST'])
+# def edit_user(_id):
+#     user = mongo.db.Users.find_one({'_id': _id})
 #     if request.method == 'POST':
 #         user['name'] = request.form.get('name')
 #         user['location'] = request.form.get('location')
 #         user['age'] = request.form.get('age')
-#         return redirect('/users')
-    
+#         mongo.db.Users.update_one({'_id': _id}, {'$set': user})
 #     return render_template('edit_user.html', user=user)
+@app.route('/edit_user/<string:_id>', methods=['GET', 'POST'])
+def edit_user(_id):
+    user = mongo.db.Users.find_one({'_id':ObjectId(_id) })
+    if request.method == 'POST':
+        user['name'] = request.form.get('name')
+        user['location'] = request.form.get('location')
+        user['age'] = request.form.get('age')
+        mongo.db.Users.update_one({'_id': ObjectId(_id)}, {'$set': user})
+        return redirect('/users_list')
+    return render_template('edit_user.html', user=user)
+
+
+
+@app.route('/delete_user/<string:_id>')
+def delete_user(_id):
+    mongo.db.Users.delete_one({'_id': ObjectId(_id)})
+    return redirect('/users')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- lab2.py
+++ lab2.py
@@ -1,80 +1,78 @@
 from flask import Flask, redirect, render_template, request
-
+from flask_pymongo import PyMongo
 app = Flask(__name__, template_folder='template')
+app.config["MONGO_URI"]="mongodb://127.0.0.1:27017/flask"
+mongo=PyMongo(app)
 
-# # models
-# users = [{"id": 1, 'name': 'mahmoud', 'age': 27, 'location': 'bani suef'},
-#          {"id": 2, 'name': 'mina', 'age': 30, 'location': 'cairo'}]
-
-
-
-#
-def get_id():
-    if users:
-        latest_user = users[-1]['id']
-        return latest_user
-    return 1
+# def get_id():
+#     users=mongo.db.mycollection.find({})
+#     if users:
+#         latest_user = users[-1]['id']
+#         return latest_user
+#     return 1
 
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+# @app.route("/")
+# def hello_world():
+#     data=mongo.db.Users.find({})
+#     print(data)
+#     return "mahmoud amr okay done"
 
 
-@app.route('/createuser')
-def createuser():
-    return render_template('createuser.html')
+# @app.route('/createuser')
+# def createuser():
+#     return render_template('createuser.html')
 
 
-@app.route('/users', methods=['GET', 'POST'])
-def adduser():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        age = request.form.get('age')
-        location = request.form.get('location')
-        users.append({'id': get_id() + 1, 'name': name, 'age': age, 'location': location})
-    return render_template('users.html', users=users)
+# @app.route('/users', methods=['GET', 'POST'])
+# def adduser():
+#     if request.method == 'POST':
+#         name = request.form.get('name')
+#         age = request.form.get('age')
+#         location = request.form.get('location')
+#         users.append({'id': get_id() + 1, 'name': name, 'age': age, 'location': location})
+#     return render_template('users.html', users=users)
 
 
 
-@app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
-def edit_user(id):
-    user = None
-    for u in users:
-        if u['id'] == id:
-            user = u
-            break
+# @app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
+# def edit_user(id):
+#     user = None
+#     for u in users:
+#         if u['id'] == id:
+#             user = u
+#             break
 
-    if user is None:
-        return 'User not found', 404
+#     if user is None:
+#         return 'User not found', 404
     
-    if request.method == 'POST':
-        user['name'] = request.form.get('name')
-        user['location'] = request.form.get('location')
-        user['age'] = request.form.get('age')
-        return redirect('/users')
+#     if request.method == 'POST':
+#         user['name'] = request.form.get('name')
+#         user['location'] = request.form.get('location')
+#         user['age'] = request.form.get('age')
+#         return redirect('/users')
     
-    return render_template('edit_user.html', user=user)
+#     return render_template('edit_user.html', user=user)
 
 
-@app.route('/delete_user/<int:id>')
-def delete_user(id):
-    if id is not None and len(users) != 0:
-        for i in range(len(users)):
-            if users[i]['id'] == id:
-                del users[i]
-                print("Found and Delete")
-                break
-    return redirect('/users')
+# @app.route('/delete_user/<int:id>')
+# def delete_user(id):
+#     if id is not None and len(users) != 0:
+#         for i in range(len(users)):
+#             if users[i]['id'] == id:
+#                 del users[i]
+#                 print("Found and Delete")
+#                 break
+#     return redirect('/users')
 
 
-@app.route('/users_list')
-def user_list():
-    return render_template('users.html', users=users)
+# @app.route('/users_list')
+# def user_list():
+#     return render_template('users.html', users=users)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 
